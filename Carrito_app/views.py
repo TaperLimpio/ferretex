4469868bd35ca1fed32b9f7e91c929db93dbffa0
TaperLimpio <167125666+TaperLimpio@users.noreply.github.com>
2024-11-12 02:03:15 -0300

    se crea el pedido, falta controlar copias y eliminar registros del carrito

--- Carrito_app/views.py
+++ Carrito_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,redirect
 from django.db.models import Sum
+from django.utils import timezone
 from .models import Carrito
 from Usuario_app.models import Usuario
 from Producto_app.models import Producto
 from Pedido_app.models import Pedido, lista_de_pedidos
+from Pedido_app.forms import PedidoForm
+
 
 def agregar_a_carrito(request,producto_id):
     id = request.session['usuario_id']
@@ -24,7 +27,7 @@ def agregar_a_carrito(request,producto_id):
 
 def ver_carrito(request,usuario_id):
     carrito = Carrito.objects.filter(id_usuario = usuario_id)
-    total = Carrito.objects.aggregate(Sum('precio'))
+    total = carrito.aggregate(Sum('precio'))
     return render(request, 'carrito.html',{'carrito':carrito,'total':total['precio__sum']})
 
 def aumentar_cantidad(request,pedido_carrito_id):
@@ -54,8 +57,41 @@ def disminuir_cantidad(request,pedido_carrito_id):
 
 def realizar_pedido(request):
     id = request.session['usuario_id']
-    pre_pedido = Pedido()
     carrito = Carrito.objects.filter(id_usuario = id)
+    form = PedidoForm()
+    if request.method == "POST":
+        print(request.POST)
+        if form.is_valid:
+            pedir(request,data = {'direccion':request.POST['direccion']})
+    return render(request,'realizar_pedido.html',{'form':form})
+
+def pedir(request,data):
+    print("se esta generando el pedido")
+    id = request.session['usuario_id']
+    carrito = Carrito.objects.filter(id_usuario = id)
+    pre_pedido = Pedido()
+    pre_pedido.repartidor = Usuario.objects.get(id = 2)
+    pre_pedido.usuario = Usuario.objects.get(id = id)
+    pre_pedido.estado = "activo"
+    pre_pedido.fechaentrega = timezone.now()
+    pre_pedido.descuento = 0.0
+    pre_pedido.direccion = data["direccion"]
+    print(carrito.aggregate(Sum('precio')))
+    pre_pedido.total = carrito.aggregate(Sum('precio'))['precio__sum']
+    pre_pedido.save()
     for car in carrito:
-        print(car.precio)
-    return redirect('mi_carrito',usuario_id = id)  
+        lista = lista_de_pedidos()
+        lista.id_pedido = pre_pedido
+        lista.id_producto = car.id_producto
+        lista.cantidad = car.cantidad
+        lista.save()
+    print("Se termino y guardo el pedido")
+    return redirect('mi_carrito',usuario_id = id)
+
+def pagoexitoso(request):
+    id = request.session['usuario_id']
+    pedido = Pedido.objects.get(usuario = id, estado = 'activo')
+    return render(request,"pagoexitoso.html",{'pedido':pedido}) 
+
+def pagofracaso(request):
+    return render(request,"pagofracaso.html")
